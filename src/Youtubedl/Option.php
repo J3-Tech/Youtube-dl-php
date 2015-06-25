ceee215069d03c930a3106fc6a7d34404b8c5813
Dickriven Chellemboyee <cd@esokia-webagency.com>
2015-06-25 09:01:43 +0400

    add option in quotes

--- src/Youtubedl/Option.php
+++ src/Youtubedl/Option.php
@@ -14,7 +14,7 @@ class Option
                 $cleanMethod = str_replace($upper, '-'.strtolower($upper), $cleanMethod);
             }
         }
-        $this->options[$cleanMethod] = current($args);
+        $this->options[$cleanMethod] = '"'.current($args).'"';
 
         return $this;
     }
