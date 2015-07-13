9c39bc500b504758111a01e73bdaa8a9da95802a
Dickriven Chellemboyee <jchellem@gmail.com>
2015-07-13 23:46:12 +0400

    update examples

--- examples/index.php
+++ examples/index.php
@@ -8,17 +8,16 @@ $youtubedl=new Youtubedl();
 $youtubedl->getOption()
         ->setOutput("'/tmp/%(title)s.%(ext)s'");
 $youtubedl->isVerbose(true)
-        ->download('BaW_jenozKc');
-
-$youtubedl->getOption()
+        ->download('BaW_jenozKc')
+        ->getOption()
         ->getListExtractors();
-echo $youtubedl->execute();
+print_r($youtubedl->execute());
 
 $youtubedl->getOption()
         ->getExtractorDescriptions();
-echo $youtubedl->execute();
+print_r($youtubedl->execute());
 
 $youtubedl->getOption()
         ->setUserAgent('Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14')
         ->dumpUserAgent();
-echo $youtubedl->execute();
+print_r($youtubedl->execute());
