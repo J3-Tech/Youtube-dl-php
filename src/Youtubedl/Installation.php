90e25144bcd9088c348f1242bec99e5c0cd932d5
Chellem <chellem@circle-economy.com>
2023-03-09 10:43:05 +0400

    fix: bin download

--- src/Youtubedl/Installation.php
+++ src/Youtubedl/Installation.php
@@ -2,8 +2,8 @@
 
 namespace Youtubedl;
 
-use React\EventLoop\Factory;
-use React\Stream\Stream;
+use React\Stream\ReadableResourceStream;
+use React\Stream\WritableResourceStream;
 use Symfony\Component\Process\Process;
 
 class Installation
@@ -21,20 +21,18 @@ class Installation
     public static function postInstall()
     {
         Config::makeBinDirectory();
-        $loop = Factory::create();
         $file = Config::getBinFile();
         $url = 'https://yt-dl.org/downloads/latest/youtube-dl';
         $readStream = fopen($url, 'r');
         $writeStream = fopen($file, 'w');
         stream_set_blocking($readStream, 0);
         stream_set_blocking($writeStream, 0);
-        $read = new Stream($readStream, $loop);
-        $write = new Stream($writeStream, $loop);
+        $read = new ReadableResourceStream($readStream);
+        $write = new WritableResourceStream($writeStream);
         $read->on('end', function() use ($file) {
             chmod($file, 0777);
             echo "Finished downloading $file\n";
         });
         $read->pipe($write);
-        $loop->run();
     }
 }
