655679dc1d83087d41fe1006ebf6bde8b650ce9a
Chellem <dickriven@chellemboyee.com>
2023-03-25 15:47:16 +0400

    add installation tests

--- tests/Youtubedl/Tests/InstallationTest.php
+++ tests/Youtubedl/Tests/InstallationTest.php
@@ -19,10 +19,31 @@ class InstallationTest extends TestCase
 
     /**
      * @test
+     */
+    public function shouldPostInstallWithoutBinary()
+    {
+        unlink(Config::getBinFile());
+        Installation::postInstall();
+        $this->assertTrue(Config::binExists());
+    }
+
+    /**
+     * @test
      * @depends shouldPostInstall
      */
     public function shouldPostUpdate()
     {
+        Installation::postUpdate();
+        $this->assertTrue(Config::binExists());
+    }
+
+    /**
+     * @test
+     * @depends shouldPostInstall
+     */
+    public function shouldPostUpdateWithoutBinary()
+    {
+        unlink(Config::getBinFile());
         Installation::postUpdate();
         $this->assertTrue(Config::binExists());
     }
